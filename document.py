e876dca32d576cc41d5e89fcc416236754eb44ef
uqjwen <wen_jiahui@outlook.com>
2019-09-20 11:49:48 +0800

    wenjh

--- document.py
+++ document.py
@@ -3,6 +3,8 @@ from docx.shared import Inches
 document = Document()
 document.add_heading('Document Title', 0)  #插入标题
 p = document.add_paragraph('A plain paragraph having some ')   #插入段落
-p.add_run('bold').bold = True
+run = p.add_run('bold').bold
+run.bold = True
+run.font.size = 20
 p.add_run(' and some ')
 p.add_run('italic.').italic = True
