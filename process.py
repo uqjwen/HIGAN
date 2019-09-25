ca43fea63c00463ad224a8df41bf4c9461af48c4
uqjwen <wen_jiahui@outlook.com>
2019-09-25 11:36:07 +0800

    wenjh

--- process.py
+++ process.py
@@ -1,6 +1,10 @@
 import numpy as np 
+import matplotlib.pyplot as plt 
 
 domain = ['Grocery_and_Gourmet_Food', 'Musical_Instruments', 'Office_Products', 'Video_Games']
+my_color = ['#107c10','#DC3C00','#7719AA','#0078D7','#DC6141','#4269A5','#39825A','#DC6141']
+
+
 
 def single_domain(filename):
 	data = np.genfromtxt(filename)
@@ -15,7 +19,34 @@ def multi_domain(domain):
 	for directory in domain:
 		single_domain('./'+directory+'/loss.txt')
 
+def plot_lines(domain):
+	for directory in domain:
+		# filename = './'+directory+'/loss.txt'
+		plot_line(directory)
+
+def get_line_cross_files(domain):
+	data = []
+	for directory in domain:
+		filename = './'+directory+'/loss.txt'
+		sub_data = np.genfromtxt(filename)
+		plt_data = sub_data[:,-1:]
+		plot_lines(plt_data)
+
+def plot_lines(data):
+	print(data.shape)
+	rows, cols = data.shape
+
+	index = [i+1 for i in range(rows)]
+	for i in range(cols):
+		sub_data = data[:,i]
+		plt.plot(index, sub_data, color = my_color[i], label = 'name '+str(i))
+	plt.grid(linestyle = '-.')
+	plt.legend()
+	plt.show()
+
+
 
 
 if __name__ == '__main__':
-	multi_domain(domain)
+	# multi_domain(domain)
+	get_line_cross_files(domain)
